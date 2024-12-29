50a55890e47ed3ec28f9b58fc7b806bfb0af4e92
Sarika Karthikeyan <sarikakarthikeyan10125@gmail.com>
2024-12-30 00:52:11 +0400

    Added files

---
+++ OneDrive/Documents/XI- IP 2023 Program/HW.py
@@ -0,0 +1,16 @@
+print('4.Display Data')
+        print('5.Search by roll no')
+        print('6.Search by name')
+        print('7.Delete by rollno')
+        print('8.Edit by rollno')
+        
+        elif ch==4:
+            fetchdata()    
+        elif ch==5:
+            searchbyrollno()
+        elif ch==6:
+            searchbyname()
+        elif ch==7:
+            deletebyrollno()
+        elif ch==8:
+            editbyrollno()    
